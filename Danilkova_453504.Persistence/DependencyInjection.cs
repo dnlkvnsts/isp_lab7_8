e9a15773f94c441dacb205531c0cc297c2a8a17b
danilkova_anastasiya <danilkova905@gmail.com>
2026-04-25 17:33:54 +0300

    feat(core): add persistence layer

---
+++ Danilkova_453504.Persistence/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Danilkova_453504.Persistence
+{
+    internal class DependencyInjection
+    {
+    }
+}
